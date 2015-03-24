b34ad8453055d38878a6047df03996ffdc2a04de
Minh Le <minhle@evolableasia.vn>
2015-03-24 17:54:03 +0700

    home + services

--- fithome_sidemenu/www/js/app.js
+++ fithome_sidemenu/www/js/app.js
@@ -30,6 +30,24 @@ angular.module('starter', ['ionic', 'starter.controllers'])
                 controller: 'AppCtrl'
             })
 
+            .state('app.home', {
+                url: "/home",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/home.html"
+                    }
+                }
+            })
+
+            .state('app.services', {
+                url: "/services",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/services.html"
+                    }
+                }
+            })
+
             .state('app.search', {
                 url: "/search",
                 views: {
@@ -67,5 +85,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
                 }
             });
         // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/app/playlists');
+        $urlRouterProvider.otherwise('/app/home');
     });
